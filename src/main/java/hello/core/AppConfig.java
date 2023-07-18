1afbd2c3c0d322523ff660d0f5bd8c112571449e
leehyeji <leehyeji95@gmail.com>
2023-07-18 13:27:07 +0900

    spring configuration

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,16 +9,21 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션의 전체 동작 방식을 구성(config)하기 위해
 // 구현 객체를 생성하고 '연결'하는 책임을 가지는 별도의 설정 클래스
 // 생성한 객체 인스턴스의 참조(레퍼런스)를 '생성자를 통해서 주입(연결)' 해준다.
+
+// @Configuration 없이 @Bean 만 등록한 경우 -> Bean 등록은 되지만, 싱글톤 보장이 되지 않는다.
+// 따라서, Spring 설정정보에는 @Configuration 넣기
 @Configuration  // 애플리케이션 구성/설정 정보
 public class AppConfig {
     // 구성영역은 변경 당연, 사용 영역은 변경 없음
-    @Bean   // Spring Container에 등록되는 메서드
+
+    @Bean   // Spring Container 에 등록되는 메서드
     public MemberService memberService() {
         // 생성자 주입을 통해 의존성 주입
         return new MemberServiceImpl(memberRepository());
@@ -26,7 +31,7 @@ public class AppConfig {
 
     // AppConfig 리팩토링 (역할과 구현의 분리)
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
         // 추후 변경 시 이 부분만 수정 -> DbMemberRepository 등...
     }
---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,49 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.annotation.Annotation;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = "+memberRepository1);
+        // + 여기서 문제 있었던 점,,
+        // AppConfig memberRepository 메서드에 static 키워드 붙어있었음 -> static 영역에 따로 생성 되는 듯
+        System.out.println("orderService -> memberRepository = "+memberRepository2);
+        System.out.println("memberRepository = "+memberRepository2);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = "+bean.getClass());
+        //@Configuration class hello.core.AppConfig$$EnhancerBySpringCGLIB$$ccc7ff19
+        //@Configuration 없으면 -> AppConfig
+    }
+}
