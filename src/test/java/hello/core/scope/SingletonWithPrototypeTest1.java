eca056f6aeb41112c9b007ad0a82b38fd8dc3e03
leehyeji <leehyeji95@gmail.com>
2023-07-31 15:22:33 +0900

    빈 스코프 - provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,24 +37,35 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입 -> 같은 프로토타입빈 사용
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입 -> 같은 프로토타입빈 사용
 
         // 프로토타입 빈 새로 생성하게 하려면,
 //        @Autowired
 //        ApplicationContext applicationContext;
 
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+
+//        DependencyLookup (DL)
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        //JSR-330 Provider 사용하기 (스프링에 의존적이지 않음)
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
 
         public int logic() {
 //            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class); // 매 시점 마다 프로토타입 빈 생성
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();  // ObjectProvider(spring framework)
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
